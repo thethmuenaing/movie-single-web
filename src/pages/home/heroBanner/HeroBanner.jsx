1005940b3887ad6a1b359bf036b6101d5eaf7d52
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-06-04 16:41:46 +0630

    layerout of movie-single-web

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+const HeroBanner = () => {
+	return (
+		<div className="heroBanner">
+			<div className="wrapper"></div>
+		</div>
+	);
+};
+
+export default HeroBanner;
