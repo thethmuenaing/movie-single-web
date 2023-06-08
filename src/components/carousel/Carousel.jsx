cec75a03cdb745e1f3511c9f09466ccb082c237f
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-06-08 21:45:24 +0630

    Genres components

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -16,6 +16,7 @@ import PosterFallback from "../../assets/no-poster.png";
 
 import "./style.scss";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 const Carousel = ({ data, loading }) => {
 	// console.log("data ", data);
@@ -60,6 +61,7 @@ const Carousel = ({ data, loading }) => {
 									<div className="posterBlock">
 										<Img src={posterUrl} />
 										<CircleRating rating={item.vote_average.toFixed(1)} />
+										<Genres data={item.genre_ids.slice(0, 2)} />
 									</div>
 									<div className="textBlock">
 										<span className="title">{item.title || item.name}</span>
