0af12c15a2a27a3ae339b5a2f137014f4090120f
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-06-11 01:20:23 +0630

    Details Page - Videos Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,6 +5,7 @@ import "./style.scss";
 import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
 
 const Details = () => {
 	const { mediaType, id } = useParams();
@@ -16,6 +17,7 @@ const Details = () => {
 		<div>
 			<DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
 			<Cast data={credits?.cast} loading={creditsLoading} />
+			<VideosSection data={data} loading={loading} />
 		</div>
 	);
 };
