551f7b53b1fdbf19b2a2fe33c27aa446e523257c
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-06-10 23:21:40 +0630

    Top Cast Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,7 @@ import useFetch from "../../hooks/useFetch";
 import "./style.scss";
 import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
 
 const Details = () => {
 	const { mediaType, id } = useParams();
@@ -14,6 +15,7 @@ const Details = () => {
 	return (
 		<div>
 			<DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+			<Cast data={credits?.cast} loading={creditsLoading} />
 		</div>
 	);
 };
