4191a65bf6546de45c4e59275372d0a7165799df
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-06-10 15:25:51 +0630

    view video trailer

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,9 +12,12 @@ import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../Playbtn";
-// import VideoPopup from "../../../components/videoPopup/VideoPopup";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
+	const [show, setShow] = useState(false);
+	const [videoId, setVideoId] = useState(null);
+
 	const { mediaType, id } = useParams();
 	const { data, loading } = useFetch(`/${mediaType}/${id}`);
 	console.log("data ", data);
@@ -23,6 +26,11 @@ const DetailsBanner = ({ video, crew }) => {
 
 	const _genres = data?.genres.map((g) => g.id);
 	// console.log("_genres ", _genres);
+
+	const viewTrailer = () => {
+		setShow(true);
+		setVideoId(video.key);
+	};
 
 	const director = crew?.filter((f) => f.job === "Director");
 	const writer = crew?.filter(
@@ -68,7 +76,7 @@ const DetailsBanner = ({ video, crew }) => {
 
 										<div className="row">
 											<CircleRating rating={data?.vote_average.toFixed(1)} />
-											<div className="playbtn" onClick={() => {}}>
+											<div className="playbtn" onClick={() => viewTrailer()}>
 												<PlayIcon />
 												<span className="text">Watch Trailer</span>
 											</div>
@@ -144,6 +152,12 @@ const DetailsBanner = ({ video, crew }) => {
 										)}
 									</div>
 								</div>
+								<VideoPopup
+									show={show}
+									setShow={setShow}
+									videoId={videoId}
+									setVideoId={setVideoId}
+								/>
 							</ContentWrapper>
 						</React.Fragment>
 					)}
